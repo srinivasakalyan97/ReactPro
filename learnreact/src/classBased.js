d8b42737df8b783d358d3c2622f3c29fdb481325
msk97 <m.srinivasakalyan123@gmail.com>
2020-05-06 09:10:59 +0530

    event handling

--- learnreact/src/classBased.js
+++ learnreact/src/classBased.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 class Greetings extends Component {
+    btnClickClassBased = () =>console.log("btn clicked class")
     render(){
         const {name , value} = this.props //Destructuring props
     return (
     <div>
     <h1>Class Based Component {name}, {value}</h1>
     {this.props.children}
+    <button onClick={this.btnClickClassBased}>Class Based btn</button>
     </div>
     )}
 }
--- learnreact/src/functionBased.js
+++ learnreact/src/functionBased.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Greet= (props)=>{
     const {name , value} = props //Destructuring props
+    const functionClick = ()=>{console.log("btn clicked function")}
     return (
     <div>
         <h1>Function Based Component {name}, {value}</h1>
         {props.children}
+        <button onClick={functionClick}>Click me Function Based</button>
     </div>
     )
     }
